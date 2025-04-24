50b6502a8248ad78527072bac7d3f75152313b30
YvesTheCode <alexandre.cleymans@gmail.com>
2025-04-24 17:59:59 +0200

    Code clean et fonctionnel

--- script.js
+++ script.js
@@ -1,32 +1,6 @@
-// function mois() {
-
-// document.write("<textarea>")
-// document.write("<table>")
-// document.write("<tr>")
-// for (let index = 1, it=0; index <= 31; ++it,++index) {
-
-//     if (it % 7 == 0) {
-//         document.write("<tr>")
-//     }
-
-//     document.write("<td>"+index+"</td>")
-
-
-// }
-// document.write("</tr>")
-// document.write("</table>")
-// document.write("</textarea>")
-
-// }
-
-
 let today = new Date(); // aujourd’hui
 let current_month = today.getMonth(); // mois courant
 let current_day = today.getDate();
-
-console.log(today)
-console.log(current_month)
-console.log(current_day)
 
 function mois(maxmois, thismonth) {
 
@@ -101,45 +75,3 @@ for (let cal = 0, moisit = 0; cal < 12; ++cal, ++moisit) {
     document.write("<br>")
     
 }
-
-// const element = document.getElementsByTagName("td")[21]
-// element.classList.add("today")
-
-
-
-
-
-// document.write("<table>")
-// for (let jour = 1; jour < 31; jour++) {
-
-
-//     document.write("<tr>")
-//     for (let semaine = 1; semaine < 8; ++semaine) {
-
-//         document.write("<td>" + jour + "</td>")
-
-//     }
-
-// document.write("</tr>")
-
-
-// }
-// document.write("</table>")
-
-// document.write("<table>")
-
-// let jour2
-// for (let semaine = 1; semaine < 8; ++semaine) {
-// document.write("<tr>")
-//     document.write("<tr>")
-//     for (let jour = 1; jour < 31; jour++) {
-//         jour2 = jour
-        
-//     }
-// document.write("<td>" + jour2 + "</td>")
-// document.write("</tr>")
-
-// }
-// document.write("</table>")
-
-//layout land
